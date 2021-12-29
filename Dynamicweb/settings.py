96be9d5ce4d40d6e822fe032fdf141dc93408bc0
siva1196 <sivaramalingam96.r@gmail.com>
2021-12-29 21:03:29 +0530

    Jinga Tags added

--- Dynamicweb/settings.py
+++ Dynamicweb/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -119,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "Static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
